3ca6b38112bc7a9436ba84eef3b030e37fa0aa1f
NathanCTR <157663583+NathanCTR@users.noreply.github.com>
2026-04-08 14:10:32 -0600

    Avance PP4

---
+++ Vehiculo.Web/Abstracciones/Login/Login.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Abstracciones.Login
+{
+    public class Login
+    {
+        [Required(ErrorMessage = "El correo es requerido")]
+        [EmailAddress(ErrorMessage = "Formato de correo inválido")]
+        public string Correo { get; set; }
+
+        [Required(ErrorMessage = "La contraseña es requerida")]
+        [StringLength(100, MinimumLength = 8, ErrorMessage = "Mínimo 8 caracteres")]
+        public string Contrasena { get; set; }
+    }
+}

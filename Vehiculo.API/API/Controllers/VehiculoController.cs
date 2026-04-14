61208dab3f7d17b84958f0c56428044088e616ce
NathanCTR <157663583+NathanCTR@users.noreply.github.com>
2026-04-14 14:31:10 -0600

    Update VehiculoController.cs

--- Vehiculo.API/API/Controllers/VehiculoController.cs
+++ Vehiculo.API/API/Controllers/VehiculoController.cs
@@ -20,7 +20,7 @@ namespace API.Controllers
         [HttpPost]
         public async Task<IActionResult> Agregar([FromBody] VehiculoRequest vehiculo)
         {
-            var resultado = _vehiculoFlujo.Agregar(vehiculo);
+            var resultado = await _vehiculoFlujo.Agregar(vehiculo);
             return CreatedAtAction(nameof(Obtener), new { Id = resultado }, null);
         }
         [HttpPut("{Id}")]
